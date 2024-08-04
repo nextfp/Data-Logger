5358cab2b1fab8674886b711327ad2534e50ad35
labFish <labFish893@gmail.com>
2024-08-05 03:44:59 +0900

    chore: ビルド用の設定を追加

--- data-logger-front/vite.config.ts
+++ data-logger-front/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 const root = path.resolve(__dirname, "./src");
+const outDir = path.resolve(__dirname, "dist");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,15 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir,
+    rollupOptions: {
+      input: {
+        main: path.resolve(root, "index.html"),
+        indicator: path.resolve(root, "indicator/index.html"),
+      },
     },
   },
 });
